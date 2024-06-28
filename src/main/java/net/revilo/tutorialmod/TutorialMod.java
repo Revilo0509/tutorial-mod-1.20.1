a69ea86d97d466f11b099bacc2003643256c8944
Revilo0509 <olivers_gamecenter@outlook.com>
2024-06-28 18:51:00 +0200

    End of E2

--- src/main/java/net/revilo/tutorialmod/TutorialMod.java
+++ src/main/java/net/revilo/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.revilo.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.revilo.tutorialmod.item.ModItemGroups;
 import net.revilo.tutorialmod.item.Moditems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,7 +13,8 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItemGroups.registerItemGroups();
+		ModItemGroups.registerItemGroups();
 		Moditems.registerModItems();
 		LOGGER.info("Initialized " + TutorialMod.MOD_ID);
 	}
